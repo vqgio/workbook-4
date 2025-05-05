5f9d18351750ad6d79427cac857c229a9fa1233c
Gio <giovannizavala82@gmail.com>
2025-05-05 14:35:12 -0500

    got room, Reservation, employee completed

--- HotelOperations/src/main/java/com/pluralsight/Employee.java
+++ HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -1,5 +1,27 @@
 package com.pluralsight;
 
 public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
 
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getRegularHoursWorked() {
+        return Math.min(hoursWorked, 40);
+    }
+    public double getOvertimeHours() {
+        return Math.max(hoursWorked - 40, 0);
+    }
+    public double getTotalHours() {
+        return (getRegularHoursWorked() * payRate) + (getOvertimeHours() + payRate * 1.50);
+    }
 }
--- HotelOperations/src/main/java/com/pluralsight/Reservation.java
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
 public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+    private double pricePerNight;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend, double pricePerNight) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+        this.pricePerNight = pricePerNight;
+        //create a method to calculate price for night
+    }
+    private void setPricePerNight() {
+        if(roomType.equalsIgnoreCase("king")) {
+            pricePerNight = 139.00;
+        } else if (roomType.equalsIgnoreCase("double")) {
+            pricePerNight = 124.00;
+        }
+        if (isWeekend) {
+            pricePerNight *= 1.10; //use this to increase price by 10%
+        }
+    }
+    public String getRoomType() {
+        return roomType;
+    }
+    public voie setRoomType(String roomType) {
+        this.roomType = roomType;
+        setPricePerNight();
+    }
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+        setPricePerNight();
+    }
+    public double getReservationTotal() {
+        return pricePerNight * numberOfNights;
+    }
 }
